cea5ad579bed71e4bb6e9ce04ce792bc58b76bec
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-04 12:49:29 -0300

    problemas com o código, pequeno impasse

--- src/main/java/br/com/duxusdesafio/repository/ComposicaoTimeRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/ComposicaoTimeRepository.java
@@ -2,6 +2,8 @@ package br.com.duxusdesafio.repository;
 
 import br.com.duxusdesafio.model.ComposicaoTime;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ComposicaoTimeRepository extends JpaRepository<ComposicaoTime, Long> {
 }
