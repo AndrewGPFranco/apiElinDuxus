f9a003600228e7bcbd680775e07d0162d2a0ab2b
Andrew Gomes <andrewgomes1328@gmail.com>
2024-02-04 15:49:34 -0300

    Revert "problemas com o código, pequeno impasse"
    This reverts commit cea5ad579bed71e4bb6e9ce04ce792bc58b76bec.

--- src/main/java/br/com/duxusdesafio/repository/ComposicaoTimeRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/ComposicaoTimeRepository.java
@@ -2,8 +2,6 @@ package br.com.duxusdesafio.repository;
 
 import br.com.duxusdesafio.model.ComposicaoTime;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ComposicaoTimeRepository extends JpaRepository<ComposicaoTime, Long> {
 }
--- src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
+++ src/main/java/br/com/duxusdesafio/repository/TimeRepository.java
@@ -2,8 +2,6 @@ package br.com.duxusdesafio.repository;
 
 import br.com.duxusdesafio.model.Time;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TimeRepository extends JpaRepository<Time, Long> {
 }
